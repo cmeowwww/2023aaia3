a5cc7638b180c970254d2e193f2eb6fee1f08a4a
cmeowwww <144758517+cmeowwww@users.noreply.github.com>
2024-11-21 16:37:11 +0800

    Add files via upload

---
+++ week11/week11-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    unordered_map<int,int> m;
+    m[999]=13; ///Hash map 雜臭表
+    m[137]=7;
+
+    cout << "m[999]的值是" << m[999] << endl;
+    cout << "m[137]的值是" << m[137] << endl;
+    return 0;
+
+
+}
